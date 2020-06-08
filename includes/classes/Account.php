7ac71167d1ca0a20a42035b86844b42da28a02e1
Thien Long <longnguyyen97@gmail.com>
2020-06-08 21:57:21 +0700

    More detailed validating validating form is not enough detail
    Added Account.php to process the validating
    Added Constants.php to declared variable for error messages
    Modified register page for more fitting validation
    
    Note

---
+++ includes/classes/Account.php
@@ -0,0 +1,106 @@
+<?php
+    class Account
+    {
+        private $conn;
+        private $errorArray = array();
+
+        public function __construct($conn)
+        {
+            $this->conn = $conn;
+        }
+
+        public function register($fn, $ln, $un, $em, $em2, $pw, $pw2)
+        {
+            $this->validateFirstName($fn);
+            $this->validateLastName($ln);
+            $this->validateUsername($un);
+            $this->validateEmails($em, $em2);
+            $this->validatePassword($pw, $pw2);
+        }
+
+        private function validateFirstName($fn)
+        {
+            if(strlen($fn) < 2 || strlen($fn) > 25) //check the length of the first name
+            {
+                //push this string into the $errorArray
+                array_push($this->errorArray, Constants::$firstNameCharacters);
+            }
+        }
+
+        private function validateLastName($ln)
+        {
+            if(strlen($ln) < 2 || strlen($ln) > 25) //check the length of the last name
+            {
+                //push this string into the $errorArray
+                array_push($this->errorArray, Constants::$lastNameCharacters);
+            }
+        }
+
+        private function validateUsername($un)
+        {
+            if(strlen($un) < 2 || strlen($un) > 15) //check the length of the username
+            {
+                //push this string into the $errorArray
+                array_push($this->errorArray, Constants::$usernameCharacters);
+                return;
+            }
+
+            $query = $this->conn->prepare("SELECT * FROM users WHERE username=:un");
+            $query->bindValue(":un", $un);
+
+            $query->execute();
+
+            if($query->rowCount() !=0)
+            {
+                array_push($this->errorArray, Constants::$usernameTaken);
+            }
+
+        }
+
+        private function validateEmails($em, $em2)
+        {
+            if($em != $em2)
+            {
+                array_push($this->errorArray, Constants::$emailsDontMatch);
+                return;
+            }
+            if(!filter_var($em, FILTER_VALIDATE_EMAIL))// filter_var will perfom a filter on a value that parsed in 
+            {
+                array_push($this->errorArray, Constants::$emailInvalid);
+            }
+
+            $query = $this->conn->prepare("SELECT * FROM users WHERE email=:em");
+            $query->bindValue(":em", $em);
+
+            $query->execute();
+
+            if($query->rowCount() !=0)
+            {
+                array_push($this->errorArray, Constants::$emailTaken);
+            }
+        }
+
+        private function validatePassword($pw, $pw2)
+        {
+            if($pw != $pw2)
+            {
+                array_push($this->errorArray, Constants::$passwordsDontMatch);
+                return;
+            }
+            if(strlen($pw) < 6) //check the length of the password
+            {
+                //push this constant into the $errorArray
+                array_push($this->errorArray, Constants::$passwordLength);
+                return;
+            }
+        }
+
+        public function getError($error)
+        {
+            if(in_array($error, $this->errorArray)) //check if $error is in the $errorArray
+            {
+                return "<span> class='errorMessage'>$error</span>";
+            }
+        }
+    }
+?>
--- register.php
+++ register.php
@@ -1,6 +1,12 @@
 <?php
     require_once("includes/classes/FormSanitizer.php");
+    require_once("includes/classes/Constants.php");
     require_once("includes/config.php");
+    require_once("includes/classes/Account.php");
+    
+    
+    $account = new Account($conn);
+
     if(isset($_POST["submitButton"]))
     {
         $firstName = FormSanitizer::sanitizeFormString($_POST["firstName"]);
@@ -10,6 +16,8 @@
         $email2 = FormSanitizer::sanitizeFormEmail($_POST["email2"]);
         $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
         $password2 = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+
+        $account->register($firstName, $lastName, $userName, $email, $email2, $password, $password2);
     }
 ?>
 <!DOCTYPE html>
@@ -31,17 +39,21 @@
                 <span>to continue to Kinoflix</span>
             </div>
             <form method="POST">
-
+                <?php echo $account->getError(Constants::$firstNameCharacters);?>
                 <input type="text" name="firstName" placeholder="First name" required>
-
+                <?php echo $account->getError(Constants::$lastNameCharacters);?>
                 <input type="text" name="lastName" placeholder="Last name" required>
-
+                <?php echo $account->getError(Constants::$usernameCharacters);?>
+                <?php echo $account->getError(Constants::$usernameTaken);?>
                 <input type="text" name="username" placeholder="Username" required>
-
+                <?php echo $account->getError(Constants::$emailsDontMatch);?>
+                <?php echo $account->getError(Constants::$emailInvalid);?>
+                <?php echo $account->getError(Constants::$emailTaken);?>
                 <input type="email" name="email" placeholder="Email" required>
 
                 <input type="email" name="email2" placeholder="Confirm Email" required>
-
+                <?php echo $account->getError(Constants::$passwordsDontMatch);?>
+                <?php echo $account->getError(Constants::$passwordLength);?>
                 <input type="password" name="password" placeholder="Password" required>
 
                 <input type="password" name="password2" placeholder="Confirm Password" required>
