ef3cd642f6a8cfe8487e011dfe02d459325a94cf
Thien Long <longnguyyen97@gmail.com>
2020-06-09 01:10:12 +0700

    formated conig.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,5 @@
 <?php
 session_start();//saving variables and values even after page is close
-
 ob_start(); //turn on output buffering (waits until all php code executed before outputting into page)
 date_default_timezone_set("Asia/Ho_Chi_Minh");// set default timezone
 try
