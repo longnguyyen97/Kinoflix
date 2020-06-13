94cd42938927aeda8cbb01b0aba4bbbfc8e09af1
Thien Long <longnguyyen97@gmail.com>
2020-06-13 23:35:05 +0700

    Play video Added video to watch page
    Note

--- watch.php
+++ watch.php
@@ -1,10 +1,16 @@
 <?php
 require_once("includes/header.php");
 
-if(!isset($_GET["id"])) {
+if(!isset($_GET["id"])) 
+{
     ErrorMessage::show("No ID passed into page");
 }
 
 $video = new Video($conn, $_GET["id"]);
 $video->incrementViews();
 ?>
+<div class="watchContainer">
+    <video controls autoplay>
+        <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4"'>
+    </video>
+</div>
